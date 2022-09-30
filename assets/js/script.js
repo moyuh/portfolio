d09c56bc76d0f2d93d03fc66fdde75b6afe3166d
moyuh <mlroth94@gmail..com>
2022-09-30 00:39:47 -0600

    initial commit lol

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+var form = document.getElementById("my-form");
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("clickStatus");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.classList.add("success");
+          status.innerHTML = "Message Sent! Thanks (:";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.classList.add("error");
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.classList.add("error");
+              status.innerHTML = "Error: Please try again :/"
+            }
+          }); 
+        }
+      }).catch(error => {
+        status.classList.add("error");
+        clickStatus.innerHTML = "Error: Please try again :/"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
